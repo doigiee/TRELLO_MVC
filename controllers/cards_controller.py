ef9636d3d7714ebacc704eddee8caca2705cf37e
doigiee <doigiee@live.com>
2022-10-25 08:53:20 +1000

    trello_mvc

---
+++ controllers/cards_controller.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from db import db
+from models.card import Card, CardSchema
+
+cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
+
+@cards_bp.route('/')
+# @jwt_required()
+def all_cards():
+    # return 'all_cards route'
+    # if not authorize():
+    #     return {'error': 'You must be an admin'}, 401
+
+    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
+    cards = db.session.scalars(stmt)
+    return CardSchema(many=True).dump(cards)
+
+@cards_bp.route('/<int:id>/')
+def one_card(id):
+    stmt = db.select(Card).filter_by(id=id)
+    card = db.session.scalar(stmt)
+    return CardSchema().dump(card)
+    
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from db import db, ma
+from controllers.cards_controller import cards_bp
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config ['JSON_SORT_KEYS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(cards_bp)
+
+    return app
